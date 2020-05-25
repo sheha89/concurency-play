14ef314b059af7c378af8b654d68b444ce908515
p0103317 <shehan.a@ncs.com.sg>
2020-05-25 09:17:54 +0800

    Added transaction domain for transaction operations

---
+++ BankingSystem/src/main/java/banking/Transaction.java
@@ -0,0 +1,44 @@
+package banking;
+
+/**
+ *
+ * Private Variables:<br>
+ * {@link #accountNumber}: Long<br>
+ * {@link #bank}: Bank<br>
+ */
+public class Transaction {
+	private Long accountNumber;
+	private Bank bank;
+
+	/**
+	 *
+	 * @param bank
+	 *            The bank where the account is housed.
+	 * @param accountNumber
+	 *            The customer's account number.
+	 * @param attemptedPin
+	 *            The PIN entered by the customer.
+	 * @throws Exception
+	 *             Account validation failed.
+	 */
+	public Transaction(Bank bank, Long accountNumber, int attemptedPin) throws Exception {
+		if(bank.authenticateUser(accountNumber, attemptedPin)) {
+			this.accountNumber = accountNumber;
+			this.bank = bank;
+		} else {
+			throw new Exception("PIN is invalid");
+		}
+	}
+
+	public double getBalance() {
+        return bank.getBalance(accountNumber);
+	}
+
+	public void credit(double amount) {
+		bank.credit(accountNumber, amount);
+	}
+
+	public boolean debit(double amount) {
+        return bank.debit(accountNumber, amount);
+	}
+}
